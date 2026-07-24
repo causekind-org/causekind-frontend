f546f5d1cfeb490a7d62e901b699011200ff8c8a
Hardeix <motiraysushil@gmail.com>
2026-07-24 16:35:34 +0530

    feat: nudge logged-out visitors to log in once, after a short delay

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,7 @@ import { CookieConsent } from "@/components/CookieConsent";
 import { WelcomeOverlay } from "@/components/WelcomeOverlay";
 import TourController from "@/components/tour/TourController";
 import { DonorListingPrompt } from "@/components/DonorListingPrompt";
+import { LoginPrompt } from "@/components/LoginPrompt";
 import { DoneeRequestPrompt } from "@/components/DoneeRequestPrompt";
 import { DoneeListingPrompt } from "@/components/DoneeListingPrompt";
 import { DonorCategoryModal } from "@/components/DonorCategoryModal";
@@ -126,6 +127,7 @@ export default async function RootLayout({
                 <DoneeListingPrompt />
                 <DonorListingPrompt />
                 <DoneeRequestPrompt />
+                <LoginPrompt />
                 </ClickSpark>
               </NotificationsProvider>
             </AuthProvider>
